7b7ed5e24b772f973d44606c0390b39a16d8f97a
yasar.kartal <yasar.kartal@arcelik.com>
2020-12-20 21:48:47 +0300

    add mutex lock

--- src/main.c
+++ src/main.c
@@ -4,16 +4,20 @@
 #include "main.h"
 
 /*shared resource*/
-int counter = 0;
+static int counter = 0;
+
+static pthread_mutex_t lock;
 
 void* threadWrite(void *threadName)
 {
     while(1)
     {
+        pthread_mutex_lock(&lock);
         printf("%s start\n", threadName);
         counter++;
         printf("%s counter: %d\n", threadName, counter);
         printf("%s finish\n", threadName);
+        pthread_mutex_unlock(&lock);
 
         msleep(THREAD_PERIOD);
     }
@@ -23,9 +27,11 @@ void* threadRead(void *threadName)
 {
     while(1)
     {
+        pthread_mutex_lock(&lock);
         printf("%s start\n", threadName);
         printf("%s counter: %d\n", threadName, counter);
         printf("%s finish\n", threadName);
+        pthread_mutex_unlock(&lock);
 
         msleep(THREAD_PERIOD);
     }
@@ -38,7 +44,14 @@ int main()
 
     printf("tread-sync-app v%s: start\n", VERSION);
 
-    /*Create thread: threadA, threadB and threadC*/
+
+    /*Init mutex*/
+    if(pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("thread-sync-app: mutex init failed\n");
+    }
+
+    /*Create threads*/
     pthread_create(&thread_write[0], NULL, threadWrite, "thread_write_A");
     pthread_create(&thread_write[1], NULL, threadWrite, "thread_write_B");
     pthread_create(&thread_write[2], NULL, threadWrite, "thread_write_C");
@@ -49,6 +62,9 @@ int main()
     pthread_join(thread_write[1], NULL);
     pthread_join(thread_write[2], NULL);
     pthread_join(thread_read[0], NULL);
+
+    /*destroy mutex*/
+    pthread_mutex_destroy(&lock);
 
     printf("main counter: %d\n",counter);
 
